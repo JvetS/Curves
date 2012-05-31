a1e46a1ac211ae7a90eac1e4cbe2e9c5007f0a62
Steven van Rossum <stevenvanrossum@gmail.com>
2012-06-01 01:04:17 +0200

    Auto update of project file.

---
+++ Curves/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Curves
+//
+//  Created by Steven Rossum on 31-05-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
